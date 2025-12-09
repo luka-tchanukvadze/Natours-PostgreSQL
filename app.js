dd09fa4b21a0f0f3230e55867e9ac9795d502e77
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-09 18:16:03 +0400

    Implementing a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -26,4 +26,14 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
