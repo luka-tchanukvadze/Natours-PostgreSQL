05536850598844275ddf380c5277599f7d665619
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-09 18:58:32 +0400

    Catching Errors in Async Functions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const pool = require('./../db');
 const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '2';
@@ -16,52 +17,47 @@ function addVirstuals(tour) {
   };
 }
 
-exports.getAllTours = async (req, res) => {
-  try {
-    const features = new APIFeatures('tours', req.query)
-      .filter()
-      .sort()
-      .fields()
-      .paginate();
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures('tours', req.query)
+    .filter()
+    .sort()
+    .fields()
+    .paginate();
 
-    const result = await pool.query(features.sql, features.values);
+  const result = await pool.query(features.sql, features.values);
 
-    if (result.rows.length === 0 && req.query.page > 1) {
-      return res.status(404).json({
-        status: 'fail',
-        message: 'This page does not exist',
-      });
-    }
-
-    const toursWithVirtuals = result.rows.map(addVirstuals);
-
-    res.status(200).json({
-      status: 'success',
-      results: result.rows.length,
-      data: { tours: toursWithVirtuals },
+  if (result.rows.length === 0 && req.query.page > 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'This page does not exist',
     });
-  } catch (err) {
-    res.status(400).json({ status: 'fail', message: err.message });
   }
-};
 
-exports.createTour = async (req, res) => {
-  try {
-    const {
-      name,
-      duration,
-      max_group_size,
-      rating,
-      ratings_quantity,
-      price,
-      summary,
-      description,
-      image_cover,
-      images,
-      start_dates,
-    } = req.body;
+  const toursWithVirtuals = result.rows.map(addVirstuals);
 
-    const sql = `
+  res.status(200).json({
+    status: 'success',
+    results: result.rows.length,
+    data: { tours: toursWithVirtuals },
+  });
+});
+
+exports.createTour = catchAsync(async (req, res, next) => {
+  const {
+    name,
+    duration,
+    max_group_size,
+    rating,
+    ratings_quantity,
+    price,
+    summary,
+    description,
+    image_cover,
+    images,
+    start_dates,
+  } = req.body;
+
+  const sql = `
       INSERT INTO tours (
         name, duration, max_group_size, rating, ratings_quantity, price,
         summary, description, image_cover, images, start_dates
@@ -70,66 +66,56 @@ exports.createTour = async (req, res) => {
       ) RETURNING *
     `;
 
-    const values = [
-      name,
-      duration,
-      max_group_size,
-      rating || 4.5,
-      ratings_quantity || 0,
-      price,
-      summary,
-      description,
-      image_cover,
-      images || [],
-      start_dates || [],
-    ];
+  const values = [
+    name,
+    duration,
+    max_group_size,
+    rating || 4.5,
+    ratings_quantity || 0,
+    price,
+    summary,
+    description,
+    image_cover,
+    images || [],
+    start_dates || [],
+  ];
 
-    const result = await pool.query(sql, values);
-    const newTour = result.rows[0];
+  const result = await pool.query(sql, values);
+  const newTour = result.rows[0];
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tours: newTour,
-      },
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: newTour,
+    },
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const sql = 'SELECT * FROM tours WHERE id = $1';
+  const value = [id];
+  const result = await pool.query(sql, value);
+  const tour = result.rows[0];
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: `No tour found with ID ${id}`,
     });
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({ status: 'fail', message: error.message });
   }
-};
 
-exports.getTour = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const sql = 'SELECT * FROM tours WHERE id = $1';
-    const value = [id];
-    const result = await pool.query(sql, value);
-    const tour = result.rows[0];
+  res.status(200).json({
+    status: 'success',
+    data: tour,
+  });
+});
 
-    if (!tour) {
-      return res.status(404).json({
-        status: 'fail',
-        message: `No tour found with ID ${id}`,
-      });
-    }
+exports.updateTour = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const { name, rating, price } = req.body;
 
-    res.status(200).json({
-      status: 'success',
-      data: tour,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({ status: 'fail', message: error.message });
-  }
-};
-
-exports.updateTour = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { name, rating, price } = req.body;
-
-    const sql = `
+  const sql = `
       UPDATE tours
       SET 
         name   = COALESCE($1, name),
@@ -138,42 +124,33 @@ exports.updateTour = async (req, res) => {
       WHERE id = $4
       RETURNING *;
     `;
-    const values = [name, rating, price, id];
-    const result = await pool.query(sql, values);
+  const values = [name, rating, price, id];
+  const result = await pool.query(sql, values);
 
-    const updatedTour = result.rows[0];
+  const updatedTour = result.rows[0];
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour: updatedTour,
-      },
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({ status: 'fail', message: error.message });
-  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: updatedTour,
+    },
+  });
+});
+
+exports.deleteTour = async (req, res, next) => {
+  const { id } = req.params;
+
+  const sql = `DELETE FROM tours WHERE id = $1`;
+  const values = [id];
+  await pool.query(sql, values);
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Tour successfully deleted',
+  });
 };
 
-exports.deleteTour = async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const sql = `DELETE FROM tours WHERE id = $1`;
-    const values = [id];
-    await pool.query(sql, values);
-
-    res.status(200).json({
-      status: 'success',
-      message: 'Tour successfully deleted',
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({ status: 'fail', message: error.message });
-  }
-};
-
-// exports.checkBody = (req, res, next) => {
+// exports.checkBody = (req, res, next, next) => {
 //   if (!req.body.name || !req.body.price || !req.body.rating) {
 //     return res.status(400).json({
 //       status: 'fail',
@@ -184,9 +161,8 @@ exports.deleteTour = async (req, res) => {
 //   next();
 // };
 
-exports.getTourStats = async (req, res) => {
-  try {
-    const sql = `
+exports.getTourStats = catchAsync(async (req, res, next) => {
+  const sql = `
       SELECT
         difficulty,
         COUNT(*) AS total_tours,
@@ -199,24 +175,19 @@ exports.getTourStats = async (req, res) => {
       GROUP BY difficulty
       ORDER BY difficulty;
     `;
-    const result = await pool.query(sql);
-    const stats = result.rows;
+  const result = await pool.query(sql);
+  const stats = result.rows;
 
-    res.status(200).json({
-      status: 'success',
-      data: stats,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({ status: 'fail', message: error.message });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    data: stats,
+  });
+});
 
-exports.getMonthlyPlan = async (req, res) => {
-  try {
-    const year = Number(req.params.year);
+exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
+  const year = Number(req.params.year);
 
-    const sql = `
+  const sql = `
       SELECT 
         EXTRACT(MONTH FROM sd) AS month,
         COUNT(*) AS num_tour_starts,
@@ -229,20 +200,13 @@ exports.getMonthlyPlan = async (req, res) => {
       LIMIT 12;
     `;
 
-    const values = [`${year}-01-01`, `${year}-12-31`];
+  const values = [`${year}-01-01`, `${year}-12-31`];
 
-    const result = await pool.query(sql, values);
+  const result = await pool.query(sql, values);
 
-    res.status(200).json({
-      status: 'success',
-      results: result.rows.length,
-      data: result.rows,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(400).json({
-      status: 'fail',
-      message: error.message,
-    });
-  }
-};
+  res.status(200).json({
+    status: 'success',
+    results: result.rows.length,
+    data: result.rows,
+  });
+});
