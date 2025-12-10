7654c39de5e10191741ce89241e1e4a67150e2ff
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-10 15:53:14 +0400

    Errors During Development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,17 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendErrorDev = (err, req, res) => {
+  console.log(err);
 
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  sendErrorDev(err, req, res);
 };
