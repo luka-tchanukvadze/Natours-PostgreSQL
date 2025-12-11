c1905870d3b3d4503966cb176abbab3d49d88f97
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-11 16:54:22 +0400

    Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log('ERROR!!!', err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -13,8 +20,8 @@ const server = app.listen(PORT, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNHANDED REJECTION! Shutting down...');
+  console.log('UNHANLED REJECTION! Shutting down...');
+  console.log('ERROR!!!', err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
