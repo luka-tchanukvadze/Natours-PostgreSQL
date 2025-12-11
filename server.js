f47866ce7545298ff8a4e89ee2b69d130ea70b65
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-11 16:48:36 +0400

    Errors Outside Express Unhandled Rejections

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const app = require('./app');
 const pool = require('./db');
 
 // test db connection
-pool
-  .query('SELECT 1')
-  .then(() => console.log('DB connected'))
-  .catch((err) => console.error(err));
+pool.query('SELECT 1').then(() => console.log('DB connected'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
