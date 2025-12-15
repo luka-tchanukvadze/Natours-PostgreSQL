319137b2b754af716de8173a798b787611846025
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-15 16:53:54 +0400

    users schema

--- database.sql
+++ database.sql
@@ -21,3 +21,23 @@ CREATE TABLE tours (
   created_at TIMESTAMP DEFAULT NOW(),
   start_dates TIMESTAMP[]
 );
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+
+    photo VARCHAR(255) DEFAULT 'default.jpg',
+    role VARCHAR(20) NOT NULL DEFAULT 'user' CHECK (role IN ('user', 'guide', 'lead-guide', 'admin')),
+
+    password VARCHAR(255) NOT NULL,
+    password_confirm VARCHAR(255) NOT NULL,
+
+    password_changed_at TIMESTAMP,
+    password_reset_token VARCHAR(255),
+    password_reset_expires TIMESTAMP,
+
+    active BOOLEAN DEFAULT TRUE
+);
+
+
