069103864d49655687b68384b0832af24c8e66f9
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-16 16:24:27 +0400

    Managing passwords

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,39 +1,64 @@
 const pool = require('./../db');
 const catchAsync = require('./../utils/catchAsync');
-
 const AppError = require('./../utils/appError');
+const bcrypt = require('bcryptjs');
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+////////  managing passwords  //////////
+
+////////////////////////////////////////
+////////////////////////////////////////
+
+const hashPasswordIfModified = async (user) => {
+  if (!user.password) return user;
+
+  user.password = await bcrypt.hash(user.password, 12);
+  user.password_confirm = null;
+
+  return user;
+};
+
+//////////////////////////////////
+//////////////////////////////////
+
+////////  http methods  //////////
+
+//////////////////////////////////
+//////////////////////////////////
 
 exports.signup = catchAsync(async (req, res, next) => {
+  const userData = await hashPasswordIfModified(req.body);
+
   const {
     name,
     email,
     photo,
     role,
     password,
-    password_confirm,
     password_changed_at,
     password_reset_token,
     password_reset_expires,
     active,
-  } = req.body;
+  } = userData;
 
   const sql = `
-  INSERT INTO users (
-    name,
-    email,
-    photo,
-    role,
-    password,
-    password_confirm,
-    password_changed_at,
-    password_reset_token,
-    password_reset_expires,
-    active
-  ) VALUES (
-    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10
-  )
-  RETURNING *;
-`;
+    INSERT INTO users (
+      name,
+      email,
+      photo,
+      role,
+      password,
+      password_changed_at,
+      password_reset_token,
+      password_reset_expires,
+      active
+    ) VALUES (
+      $1, $2, $3, $4, $5, $6, $7, $8, $9
+    )
+    RETURNING *;
+  `;
 
   const values = [
     name,
@@ -41,7 +66,6 @@ exports.signup = catchAsync(async (req, res, next) => {
     photo,
     role,
     password,
-    password_confirm,
     password_changed_at,
     password_reset_token,
     password_reset_expires,
