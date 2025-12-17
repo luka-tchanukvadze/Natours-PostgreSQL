d9ffddbdbb24c6a98334baf5e5a81f0913dbf213
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-17 18:06:49 +0400

    Get all Users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,50 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+const pool = require('./../db');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const sql = `SELECT * FROM users`;
+  const result = await pool.query(sql);
+
+  if (!result.rows.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No users',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: result.rows.length,
+    data: {
+      users: result.rows,
+    },
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
 exports.getUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
 exports.deleteUser = (req, res) => {
   res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
