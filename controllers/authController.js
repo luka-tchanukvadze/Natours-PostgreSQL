af33277e3d3c7d8d95b687a84f0c424a2d103591
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-23 16:30:32 +0400

    Sending Emails with nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -246,16 +246,26 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   const message = `Forgot your password? Submit a PATCH request with your new passwod and passwordCOnfirm to: ${resetURL}/\n if you didn't forget your password, please ignore this email!`;
 
-  await sendEmail({
-    email: user.email,
-    subject: 'Your password reset token (valid for 10min)',
-    message,
-  });
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10min)',
+      message,
+    });
 
-  res.status(200).json({
-    status: 'success',
-    message: 'Token sent to email',
-  });
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email',
+    });
+  } catch (error) {
+    console.log(error);
+    currentUser.password_reset_token = undefined;
+    currentUser.password_reset_expires = undefined;
+
+    return next(
+      new AppError('There was an error sending the email.Try again later!', 500)
+    );
+  }
 });
 
 exports.resetPassword = (req, res, next) => {};
