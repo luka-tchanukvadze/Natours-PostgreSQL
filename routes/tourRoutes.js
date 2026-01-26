84d4c53d11b69cd66e6576b9c9b9e26f2e620411
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-01-26 21:06:29 +0400

    Geospatial Queries Finding Tours Within Radius

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -19,6 +19,15 @@ router
     tourController.getMonthlyPlan,
   );
 
+/////////////
+// /tours-distance?distance=233,center=-40,45&unit=mi
+// /tours-distance/233/center/-40,45/unit/mi
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/')
   .get(tourController.getAllTours)
