abe50a45785797acd1cbd1f597d6e2af20179758
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-02-05 19:25:32 +0400

    drop CommonJS and fix TypeScript scripts

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,19 @@
-declare namespace Express {
-  export interface Request {
-    requestTime?: string;
+// types.d.ts
+import { Request } from 'express';
+
+declare global {
+  namespace Express {
+    interface User {
+      id: number;
+      name: string;
+      email: string;
+      role: string;
+      password_changed_at: Date;
+    }
+
+    export interface Request {
+      requestTime?: string;
+      user?: User; // Add the user property
+    }
   }
 }
