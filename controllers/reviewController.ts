3fa334b81c7ceaf278045ff6d9247abdfa124b5c
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2026-02-08 16:17:49 +0400

    TypeScript Migration

--- controllers/reviewController.ts
+++ controllers/reviewController.ts
@@ -1,15 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
 import pool from './../db.js';
 import catchAsync from './../utils/catchAsync.js';
 import AppError from './../utils/appError.js';
 import * as factory from './handlerFactory.js';
+import { Review } from './../types';
 
 /**
  * Allow nested routes
  * /tours/:tourId/reviews
  */
-export const setTourUserIds = (req, res, next) => {
+export const setTourUserIds = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
   if (!req.body.tour_id) req.body.tour_id = req.params.tourId;
-  if (!req.body.user_id) req.body.user_id = req.user.id;
+  if (!req.body.user_id) req.body.user_id = req.user?.id;
   next();
 };
 
@@ -23,8 +29,9 @@ export const updateReview = factory.updateOne('reviews', ['review', 'rating']);
 export const getReview = factory.getOne('reviews');
 export const deleteReview = factory.deleteOne('reviews');
 
-export const getAllReviews = catchAsync(async (req, res, next) => {
-  let sql = `
+export const getAllReviews = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let sql = `
     SELECT
       r.*,
       u.name AS user_name,
@@ -32,23 +39,24 @@ export const getAllReviews = catchAsync(async (req, res, next) => {
     FROM reviews r
     JOIN users u ON r.user_id = u.id
   `;
-  const values = [];
+    const values: string[] = [];
 
-  if (req.params.tourId) {
-    sql += ` WHERE r.tour_id = $1`;
-    values.push(req.params.tourId);
-  }
+    if (req.params.tourId) {
+      sql += ` WHERE r.tour_id = $1`;
+      values.push(req.params.tourId);
+    }
 
-  const result = await pool.query(sql, values);
+    const result = await pool.query(sql, values);
 
-  res.status(200).json({
-    status: 'success',
-    results: result.rows.length,
-    data: {
-      reviews: result.rows,
-    },
-  });
-});
+    res.status(200).json({
+      status: 'success',
+      results: result.rows.length,
+      data: {
+        reviews: result.rows as Review[],
+      },
+    });
+  },
+);
 
 // export const getReview = catchAsync(async (req, res, next) => {
 //   const sql = `
